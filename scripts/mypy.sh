a217b8bcf46ae043930992c89a2dcb861ecd1130
coscialp <corentin.scialpi@gmail.com>
2023-05-16 18:25:28 +0200

    [Feat] add script to launch mypy and pylint with the good config file

---
+++ scripts/mypy.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# Run mypy on all python files in the project
+mypy --config-file gui/mypy.ini gui/
---
+++ scripts/pylint.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# Run pylint on all python files in the project
+pylint --rcfile=gui/.pylintrc gui/
